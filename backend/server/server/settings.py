8e06286faa1ab7059a11f95d9ac1771afeb06ece
jk-karthik <52814047+jk-karthik@users.noreply.github.com>
2020-09-01 01:53:27 +0530

    Update settings.py

--- backend/server/server/settings.py
+++ backend/server/server/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 SECRET_KEY = 'cqeuvh$so#y!xr3u+%e9s170((6@tcg*y%15-9opgij^s8_i9p'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
